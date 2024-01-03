115c2f91faf6aa9284fce3e0be77293c4d1c42d1
Sainadh Jaladi <110282195+sainadhjaladi@users.noreply.github.com>
2024-01-03 11:26:58 +0530

    Update Nothing.java

--- Nothing.java
+++ Nothing.java
@@ -3,6 +3,6 @@ public class Nothing {
     public static void main(String args[])
     {
         System.out.println("Hello world");
-        System.out.println("Managing jenkins");
+        System.out.println("Managing jenkins testing");
     }
 }
